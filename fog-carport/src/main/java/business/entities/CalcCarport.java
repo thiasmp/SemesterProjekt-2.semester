fa3a9fabeaf014fd6dc75eaaf302775ae4a7dc5e
Mathias Jensen <cph-mj839@chpbusiness.dk>
2021-05-14 12:25:33 +0200

    user story 2 done?

--- fog-carport/src/main/java/business/entities/CalcCarport.java
+++ fog-carport/src/main/java/business/entities/CalcCarport.java
@@ -17,10 +17,60 @@ public class CalcCarport {
     }
 
     public Result calcRafter(int length, int width) {
-
         int quantity = length/55;
         int rafterLength = width;
         int id = 12;
-        return new Result(rafterLength, quantity, 12);
+        return new Result(rafterLength, quantity, id);
+    }
+
+    public Result calcPlastmo(int length, int width){
+        int plastmoWidth = 109;
+        int plastmoLength = width;
+        int amount = (int) Math.ceil((float)length/plastmoWidth);
+        int overlap = (amount - 1) * 20;
+        int totalLength = overlap + length;
+        int quantity = (int) Math.ceil((float)totalLength/plastmoWidth);
+        int id = 16;
+        return new Result(plastmoLength, quantity, id);
+    }
+
+    public Result calcPostbolts(int length) {
+        int boltLength = 12;
+        Result posts = calcPosts(length);
+        int quantity = posts.getQuantity() * 2;
+        int id = 23;
+        return new Result(boltLength, quantity, id);
+    }
+
+    public Result calcSquareDiscs(int length) {
+        int discSize = 4;
+        Result bolts = calcPostbolts(length);
+        int quantity = bolts.getQuantity();
+        int id = 24;
+        return new Result(discSize, quantity, id);
+    }
+
+    public Result calcUniRight(int length, int width) {
+        int uniSize = 19;
+        Result rafters = calcRafter(length, width);
+        int quantity = rafters.getQuantity();
+        int id = 19;
+        return new Result(uniSize, quantity, id);
+    }
+
+    public Result calcUniLeft(int length, int width) {
+        int uniSize = 19;
+        Result rafters = calcRafter(length, width);
+        int quantity = rafters.getQuantity();
+        int id = 20;
+        return new Result(uniSize, quantity, id);
+    }
+
+    public Result calcPlastmoBolt(int length, int width) {
+        int boltSize = 0;
+        Result plastmo = calcPlastmo(length, width);
+        int quantity = (int) Math.ceil((float)plastmo.getQuantity()/4);
+        int id = 17;
+        return new Result(boltSize, quantity, id);
     }
 }
--- fog-carport/src/main/java/web/commands/CalcCarportCommand.java
+++ fog-carport/src/main/java/web/commands/CalcCarportCommand.java
@@ -31,18 +31,43 @@ public class CalcCarportCommand extends CommandProtectedPage {
         String postDesc = "Stolper nedgraves 90 cm. i jord";
         String beamDesc = "Remme i sider, sadles ned i stolper";
         String rafterDesc = "Spær, monteres på rem";
+        String plastmoDesc = "Tagplader monteres på spær";
+        String boltDesc = "Til montering af rem på stolper";
+        String squareDiscDesc = "Til montering af rem på stolper";
+        String uniRightDesc = "Til montering af spær på rem";
+        String uniLeftDesc = "Til montering af spær på rem";
+        String plastmoBoltDesc = "Skruer til tagplader";
 
         Result post = calcCarport.calcPosts(length);
         Result beam = calcCarport.calcBeams(length);
         Result rafter = calcCarport.calcRafter(length, width);
+        Result plastmo = calcCarport.calcPlastmo(length, width);
+        Result bolt = calcCarport.calcPostbolts(length);
+        Result squareDisc = calcCarport.calcSquareDiscs(length);
+        Result uniRight = calcCarport.calcUniRight(length, width);
+        Result uniLeft = calcCarport.calcUniLeft(length, width);
+        Result plastmoBolt = calcCarport.calcPlastmoBolt(length, width);
+
 
         CarportItem posts = new CarportItem(post.getLength(), post.getQuantity(), 0 , postDesc, post.getId());
         CarportItem beams = new CarportItem(beam.getLength(), beam.getQuantity(), 0 , beamDesc, beam.getId());
         CarportItem rafters = new CarportItem(rafter.getLength(), rafter.getQuantity(), 0, rafterDesc, rafter.getId());
+        CarportItem plastmos = new CarportItem(plastmo.getLength(), plastmo.getQuantity(), 0, plastmoDesc, plastmo.getId());
+        CarportItem bolts = new CarportItem(bolt.getLength(), bolt.getQuantity(), 0, boltDesc, bolt.getId());
+        CarportItem squareDiscs = new CarportItem(squareDisc.getLength(), squareDisc.getQuantity(), 0, squareDiscDesc, squareDisc.getId());
+        CarportItem uniRights = new CarportItem(uniRight.getLength(), uniRight.getQuantity(), 0, uniRightDesc, uniRight.getId());
+        CarportItem uniLefts = new CarportItem(uniLeft.getLength(), uniLeft.getQuantity(), 0, uniLeftDesc, uniLeft.getId());
+        CarportItem plastmoBolts = new CarportItem(plastmoBolt.getLength(), plastmoBolt.getQuantity(), 0 ,plastmoBoltDesc, plastmoBolt.getId());
 
         billOfMaterials.addItem(posts);
         billOfMaterials.addItem(beams);
         billOfMaterials.addItem(rafters);
+        billOfMaterials.addItem(plastmos);
+        billOfMaterials.addItem(bolts);
+        billOfMaterials.addItem(squareDiscs);
+        billOfMaterials.addItem(uniRights);
+        billOfMaterials.addItem(uniLefts);
+        billOfMaterials.addItem(plastmoBolts);
 
         for (CarportItem c: billOfMaterials.getMaterialList()) {
             carportFacade.writeToOrderline(id,c.getId(), c.getDescription(), c.getQuantity(), c.getLength());
@@ -53,7 +78,7 @@ public class CalcCarportCommand extends CommandProtectedPage {
         session.setAttribute("billOfMaterials", materialList);
         session.setAttribute("længde", length);
         session.setAttribute("bredde", width);
-        
+
         return pageToShow;
     }
 }
